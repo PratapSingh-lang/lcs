ce45153451399c203dd0086e658e50dabe7d8806
Bhanu Pratap <bhanu.pratap@subex.com>
2022-11-18 17:12:57 +0530

    adede lcs

---
+++ bhanu/src/bhanu/first_program.java
@@ -0,0 +1,11 @@
+package bhanu;
+
+public class first_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("om namah Shiwayy");
+
+	}
+
+}
